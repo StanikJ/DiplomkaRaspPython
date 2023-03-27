bfe5ab7109f603c6e21f0698e66f468d11b72870
StanikJ <jozef.stanik11@gmail.com>
2023-03-27 23:12:50 +0200

    Added turn off whole drawer button to details.html and made it prettier

--- dataToTable.py
+++ dataToTable.py
@@ -70,13 +70,23 @@ def get_details(id):
 @app.route('/details/<int:id>', methods=['POST'])
 def post_details(id):
     if session.get('logged_in'):
-        drawer = Data.query.get(id)
-        drawer.drawer1 = convert_to_number(request.form.get('drawer1'))
-        drawer.drawer2 = convert_to_number(request.form.get('drawer2'))
-        db.session.commit()
-        drawerMac = Data.query.filter_by(id=id).first().MACaddr
-        flash(f"Zasuvka s MAC adresou: |{drawerMac}|, bola uspesne updatnuta!", category='popup')
-        return redirect(url_for('drawers'))
+        action = request.form.get('action')
+        if action == 'Update':
+            drawer = Data.query.get(id)
+            drawer.drawer1 = convert_to_number(request.form.get('drawer1'))
+            drawer.drawer2 = convert_to_number(request.form.get('drawer2'))
+            db.session.commit()
+            drawerMac = Data.query.filter_by(id=id).first().MACaddr
+            flash(f"Zasuvka s MAC adresou: |{drawerMac}|, bola uspesne updatnuta!", category='popup')
+            return redirect(url_for('drawers'))
+        elif action == 'Vypnut celu zasuvku':
+            drawer = Data.query.get(id)
+            drawer.drawer1 = 0
+            drawer.drawer2 = 0
+            db.session.commit()
+            drawerMac = Data.query.filter_by(id=id).first().MACaddr
+            flash(f"Zasuvka s MAC adresou: |{drawerMac}|, bola uplne vypnuta!", category='popup')
+            return redirect(url_for('drawers'))
     else: 
         flash("Boli ste odhlaseny. Musite sa znova prihlasit.", category='popup')
         return redirect(url_for('get_login'))
